16c4efb7c572c0de0493b380daecbde93c6566b9
Mark Sherriff <sherriff@virginia.edu>
2023-02-26 15:54:06 -0500

    all courses update

---
+++ reformat.py
@@ -0,0 +1,25 @@
+
+courses_file = open("courses_raw.txt", "r")
+
+course_number = ""
+course_title = ""
+credits = ""
+description = ""
+
+for line in courses_file:
+
+    if line.startswith("CS"):
+        paren_open = line.index("(")
+        paren_close = line.index(")")
+        course_number = line[0:7]
+        course_title = line[8:paren_open]
+        credits = line[paren_open+1:paren_close]
+
+    else: 
+        description = line[1:]
+        print("- course: " + course_number)
+        print("  title: " + course_title)
+        print("  credits: " + credits)
+        print("  description: \"" + description + "\"") 
+        print("  prereqs: None")
+        print()
